95140110327f5fc94b0dba7a6c7f82c793d5c158
Guilherme <guilherme.a.welter@gmail.com>
2017-10-31 21:43:43 -0200

    Criando tela de login e chat

---
+++ app/src/main/java/br/com/gwelter/smack/LoginActivity.kt
@@ -0,0 +1,21 @@
+package br.com.gwelter.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        fun loginloginButtonClick(view: View) {
+
+        }
+
+        fun loginCreateUserButtonClick(view: View) {
+
+        }
+    }
+}
