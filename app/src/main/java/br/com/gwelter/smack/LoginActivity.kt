d3d2cd12b0e3d73662cfca7440d2283124577b0e
Guilherme <guilherme.a.welter@gmail.com>
2017-11-01 07:01:40 -0200

    Criando tela de cadastro

--- app/src/main/java/br/com/gwelter/smack/LoginActivity.kt
+++ app/src/main/java/br/com/gwelter/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package br.com.gwelter.smack
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,13 +10,14 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
 
-        fun loginloginButtonClick(view: View) {
+    fun loginloginButtonClick(view: View) {
 
-        }
+    }
 
-        fun loginCreateUserButtonClick(view: View) {
-
-        }
+    fun loginCreateUserButtonClick(view: View) {
+        val createUserIntent = Intent(this, CreateUserActivity::class.java)
+        startActivity(createUserIntent)
     }
 }
--- app/src/main/java/br/com/gwelter/smack/MainActivity.kt
+++ app/src/main/java/br/com/gwelter/smack/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.com.gwelter.smack
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -11,6 +13,9 @@ import android.view.MenuItem
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.view.inputmethod.InputMethodManager
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +30,17 @@ class MainActivity : AppCompatActivity() {
         toggle.syncState()
     }
 
+    private fun hideKeyboard(){
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
     override fun onBackPressed() {
+        hideKeyboard()
+
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
         } else {
@@ -34,7 +49,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun logginButtonNavClick(view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClick(view: View) {
