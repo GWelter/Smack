f2f977cb0163bdcf8294e1adf55b4ba1ce8eb83d
Guilherme <guilherme.a.welter@gmail.com>
2017-11-01 07:17:48 -0200

    Gerando avatar para usuário

--- app/src/main/java/br/com/gwelter/smack/CreateUserActivity.kt
+++ app/src/main/java/br/com/gwelter/smack/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package br.com.gwelter.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,32 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        userAvatar = if(color == 0){
+            "light$avatar"
+        }else{
+            "dark$avatar"
+        }
+
+        val resourceID = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageview.setImageResource(resourceID)
     }
 
     fun generateColorClick(view: View) {
+        val random = Random()
+        val red = random.nextInt(255)
+        val green = random.nextInt(255)
+        val blue = random.nextInt(255)
 
+        createAvatarImageview.setBackgroundColor(Color.rgb(red, green, blue))
+        val savedRed = red.toDouble() / 255
+        val savedGreen = green.toDouble() / 255
+        val savedBlue = blue.toDouble() / 255
+
+        avatarColor = "[$savedRed, $savedGreen, $savedBlue, 1]"
     }
 
     fun createUserClick(view: View) {
