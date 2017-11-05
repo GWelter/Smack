47b01f587d8b3bf3fc8e1f974db5b02a1ddb6686
Guilherme <guilherme.a.welter@gmail.com>
2017-11-05 11:34:11 -0200

    Nova forma de esconder o teclado e adicionando spinner ao processo de login

--- app/src/main/java/br/com/gwelter/smack/Controller/LoginActivity.kt
+++ app/src/main/java/br/com/gwelter/smack/Controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package br.com.gwelter.smack.Controller
 
+import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import br.com.gwelter.smack.R
 import br.com.gwelter.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,21 +16,41 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginloginButtonClick(view: View) {
+        enableSpinner(true)
+        hideKeyboard()
         val email = loginEmailText.text.toString()
         val password = loginPasswordText.text.toString()
 
-        AuthService.loginUser(this, email, password) { loginSuccess ->
-            println(loginSuccess)
-            if(loginSuccess){
-                AuthService.findUserByEmail(this) { findUserSuccess ->
-                    if(findUserSuccess) {
-                        finish()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) { findUserSuccess ->
+                        if (findUserSuccess) {
+                            enableSpinner(false)
+                            finish()
+                        } else {
+                            errorToast()
+                        }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Please fill in both email and password", Toast.LENGTH_SHORT).show()
+            enableSpinner(false)
+        }
+    }
+
+    private fun hideKeyboard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken, 0)
         }
     }
 
@@ -35,5 +58,21 @@ class LoginActivity : AppCompatActivity() {
         val createUserIntent = Intent(this, CreateUserActivity::class.java)
         startActivity(createUserIntent)
         finish()
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again.", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if(enable) {
+            loginSpinner.visibility = View.VISIBLE
+        } else {
+            loginSpinner.visibility = View.INVISIBLE
+        }
+
+        loginloginButton.isEnabled = !enable
+        loginCreateUserButton.isEnabled = !enable
     }
 }
